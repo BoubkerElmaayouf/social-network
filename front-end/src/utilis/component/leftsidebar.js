3527b55702d68553ad61c64abe4b4ccf008ba9bb
aminehabchi <amine.habchi.01@gmail.com>
2025-03-04 11:42:28 +0000

    follow

--- front-end/src/utilis/component/leftsidebar.js
+++ front-end/src/utilis/component/leftsidebar.js
@@ -10,6 +10,7 @@ import {
   faBell,
   faUserFriends,
 } from "@fortawesome/free-solid-svg-icons";
+import { Notification } from './notification';
 import "./css/leftsidebar.css"
 
 
@@ -20,7 +21,7 @@ import "./css/leftsidebar.css"
 export function Leftsidebar() {
   const [isCollapsed, setIsCollapsed] = useState(true);
   const [notificationActive, setNotificationActive] = useState(false);
-  const [activeTab, setActiveTab] = useState('group-invitation');
+  
   const notificationRef = useRef(null);
   const notificationBtnRef = useRef(null);
 
@@ -35,9 +36,9 @@ export function Leftsidebar() {
   useEffect(() => {
     function handleClickOutside(event) {
       if (
-        notificationRef.current && 
+        notificationRef.current &&
         !notificationRef.current.contains(event.target) &&
-        notificationBtnRef.current && 
+        notificationBtnRef.current &&
         !notificationBtnRef.current.contains(event.target)
       ) {
         setNotificationActive(false);
@@ -50,9 +51,7 @@ export function Leftsidebar() {
     };
   }, []);
 
-  const handleTabClick = (tabId) => {
-    setActiveTab(tabId);
-  };
+ 
 
   const handleNotificationClick = (e) => {
     e.preventDefault();
@@ -88,9 +87,9 @@ export function Leftsidebar() {
           <nav className="sidebar-nav">
             {menuItems.map((item, index) => (
               item.id === "notification-btn" ? (
-                <a 
-                  key={index} 
-                  href="#" 
+                <a
+                  key={index}
+                  href="#"
                   className="nav-item"
                   onClick={handleNotificationClick}
                   ref={notificationBtnRef}
@@ -112,96 +111,7 @@ export function Leftsidebar() {
       </aside>
 
       {/* Notification Container */}
-      <div ref={notificationRef} className={`notification-container ${notificationActive ? 'active' : ''}`}>
-        <div className="notification-tabs">
-          <button 
-            className={`notification-tab ${activeTab === 'group-invitation' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('group-invitation')}
-          >
-            Groups Notifications
-          </button>
-          <button 
-            className={`notification-tab ${activeTab === 'follow-requests' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('follow-requests')}
-          >
-            Follow Requests
-          </button>
-          <button 
-            className={`notification-tab ${activeTab === 'group-events' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('group-events')}
-          >
-            Group Events
-          </button>
-        </div>
-        <div className="notification-content">
-          <div className={`tab-panel ${activeTab === 'group-invitation' ? 'active' : ''}`} id="group-invitation">
-            <div className='notification-divider'>
-            <div className="group-invitation">
-              <p className="group-invitation-message">The group is inviting you! Please check it out.</p>
-              <Link href="#" className="group-invitation-link">View Invitation</Link>
-            </div>
-
-            <div className="group-invitation">
-              <p className="group-invitation-message">This profile want to join your group</p>
-              <div className="notification-group-actions">
-                  <button className="group-action-btn attend-btn">Attend</button>
-                  <button className="group-action-btn ignore-btn">Ignore</button>
-              </div>
-            </div>
-            </div>
- 
-          </div>
-          <div className={`tab-panel ${activeTab === 'follow-requests' ? 'active' : ''}`} id="follow-requests">
-            <div className="follow-request">
-              <div className="follow-avatar">
-                <div className="follow-avatar-placeholder">
-                  <img className='follow-avatar' 
-                  src="/default-img.jpg" 
-                  alt="User Avatar" />
-                </div>
-              </div>
-              <div className="follow-info">
-                <div className="follow-name">User Name</div>
-                <div className="follow-username">@username</div>
-              </div>
-              <Link href="/profile" className="visit-account">
-              Visit
-              </Link>
-            </div>
-            <div className="follow-request">
-              <div className="follow-avatar">
-                <div className="follow-avatar-placeholder">
-                <img className='follow-avatar' 
-                  src="/default-img.jpg" 
-                  alt="User Avatar" />
-                </div>
-              </div>
-              <div className="follow-info">
-                <div className="follow-name">Jane Smith</div>
-                <div className="follow-username">@janesmith</div>
-              </div>
-              <Link href="/profile" className="visit-account">
-              Visit
-              </Link>
-            </div>
-          </div>
-          <div className={`tab-panel ${activeTab === 'group-events' ? 'active' : ''}`} id="group-events">
-            <div className="notification-group-event">
-              <div className="notification-group-image">
-                {/* Placeholder for group image */}
-              </div>
-              <div className="notification-group-info">
-                <div className="notification-group-name">Gaming Group</div>
-                <div className="notification-group-event-type">Weekend Tournament</div>
-                <div className="notification-group-actions">
-                  <button className="group-action-btn attend-btn">Attend</button>
-                  <button className="group-action-btn ignore-btn">Ignore</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Notification notificationRef={notificationRef} notificationActive={notificationActive}  />
     </>
   );
 }
--- front-end/src/utilis/component/notification.js
+++ front-end/src/utilis/component/notification.js
@@ -1,7 +1,275 @@
-export function notification() {
-    return (
-        <div>
-            <h1>Notification</h1>
+"use client";
+
+import Link from "next/link.js";
+import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
+export function Notification({ notificationRef, notificationActive }) {
+  const [activeTab, setActiveTab] = useState("group-invitation");
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
+  return (
+    <>
+      <div
+        ref={notificationRef}
+        className={`notification-container ${
+          notificationActive ? "active" : ""
+        }`}
+      >
+        <div className="notification-tabs">
+          <button
+            className={`notification-tab ${
+              activeTab === "group-invitation" ? "active" : ""
+            }`}
+            onClick={() => handleTabClick("group-invitation")}
+          >
+            Groups Notifications
+          </button>
+
+          <button
+            className={`notification-tab ${
+              activeTab === "follow-requests" ? "active" : ""
+            }`}
+            onClick={() => handleTabClick("follow-requests")}
+          >
+            Follow Requests
+          </button>
+
+          <button
+            className={`notification-tab ${
+              activeTab === "group-events" ? "active" : ""
+            }`}
+            onClick={() => handleTabClick("group-events")}
+          >
+            Group Events
+          </button>
         </div>
-    )
+        <div className="notification-content">
+          <GroupsNotif activeTab={activeTab} />
+          <FollowRequest activeTab={activeTab} />
+          <GroupsEvent activeTab={activeTab} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function GroupsNotif({ activeTab }) {
+  const [elements, setElements] = useState([]);
+
+  const addElement = (data) => {
+    if (!Array.isArray(data)) return;
+
+    const elementsArray = data.map((d) => {
+      if (d.Type == "") {
+        let grouplink = "/group/" + d.Group.Id;
+        return (
+          <div key={d.Sender.Id} className="group-invitation">
+            <p className="group-invitation-message">
+              {d.Group.Title} is inviting you! Please check it out.
+            </p>
+            <Link href={grouplink} className="group-invitation-link">
+              View Invitation
+            </Link>
+          </div>
+        );
+      } else {
+        let name = d.Sender.FirstName + " " + d.Sender.LastName;
+        return (
+          <div key={d.Sender.Id} className="group-invitation">
+            <p className="group-invitation-message">
+              {name} want to join your group
+            </p>
+            <div className="notification-group-actions">
+              <button className="group-action-btn attend-btn">Attend</button>
+              <button className="group-action-btn ignore-btn">Ignore</button>
+            </div>
+          </div>
+        );
+      }
+    });
+
+    setElements(elementsArray); // Set the array of JSX elements to the state
+  };
+
+  useEffect(() => {
+    if (activeTab === "group-invitation") {
+      async function FetchData() {
+        const result = await FetchNotif("group-invitation");
+        console.log(result);
+
+        addElement(result);
+      }
+      FetchData();
+    }
+  }, [activeTab]);
+
+  return (
+    <div
+      className={`tab-panel ${
+        activeTab === "group-invitation" ? "active" : ""
+      }`}
+      id="group-invitation"
+    >
+      <div className="notification-divider">{elements} </div>
+    </div>
+  );
+}
+
+export function FollowRequest({ activeTab }) {
+  const [elements, setElements] = useState([]);
+
+  const addElement = (data) => {
+    if (!Array.isArray(data)) return;
+
+    const elementsArray = data.map((d) => {
+      d.Path = "/" + d.Path;
+      let profile = "/profile/" + d.Sender.Id;
+      let name = d.Sender.FirstName + " " + d.Sender.LastName;
+
+      return (
+        <div key={d.Sender.Id} className="follow-request">
+          <div className="follow-avatar">
+            <div className="follow-avatar-placeholder">
+              <img
+                className="follow-avatar"
+                src={d.Sender.Path}
+                alt="User Avatar"
+              />
+            </div>
+          </div>
+          <div className="follow-info">
+            <div className="follow-name">{name}</div>
+            <div className="follow-username">{d.Sender.Nickname}</div>
+          </div>
+          <Link href={profile} className="visit-account">
+            Visit
+          </Link>
+        </div>
+      );
+    });
+    setElements(elementsArray); // Set the array of JSX elements to the state
+  };
+
+  useEffect(() => {
+    if (activeTab === "follow-requests") {
+      async function FetchData() {
+        try {
+          const result = await FetchNotif("follow-requests");
+          addElement(result);
+        } catch (error) {
+          console.error("Error fetching follow requests:", error);
+        }
+      }
+      FetchData();
+    } else {
+      setElements([]); // Reset the elements array when the tab changes
+    }
+  }, [activeTab]);
+
+  return (
+    <div
+      className={`tab-panel ${activeTab === "follow-requests" ? "active" : ""}`}
+    >
+      {elements}{" "}
+    </div>
+  );
+}
+
+export default function GroupsEvent({ activeTab }) {
+  const [elements, setElements] = useState([]);
+
+  const addElement = (data) => {
+    if (!Array.isArray(data)) return;
+
+    const elementsArray = data.map((d) => {
+      let name = d.Sender.FirstName + " " + d.Sender.LastName;
+      let grouplink = "/group/" + d.Group.Id;
+      return (
+        <div key={d.Sender.Id} className="notification-group-event">
+          <div className="notification-group-image">
+            {/* Placeholder for group image */}
+          </div>
+          <div className="notification-group-info">
+            <div className="notification-group-name">{d.Group.Title}</div>
+            <div className="notification-group-event-type">{name}</div>
+            <div className="notification-group-actions">
+              <Link href={grouplink} className="group-invitation-link">
+                View Invitation
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    setElements(elementsArray); // Set the array of JSX elements to the state
+  };
+
+  useEffect(() => {
+    if (activeTab === "follow-requests") {
+      async function FetchData() {
+        try {
+          const result = await FetchNotif("follow-requests");
+          addElement(result);
+          console.log(result);
+        } catch (error) {
+          console.error("Error fetching follow requests:", error);
+        }
+      }
+      FetchData();
+    } else {
+      setElements([]); // Reset the elements array when the tab changes
+    }
+  }, [activeTab]);
+
+  return (
+    <div
+      className={`tab-panel ${activeTab === "group-events" ? "active" : ""}`}
+      id="group-events"
+    >
+      {elements}{" "}
+    </div>
+  );
+}
+
+async function FetchNotif(type) {
+  let url = "http://localhost:8080/api/notif/";
+
+  switch (type) {
+    case "group-invitation":
+      url += "invitations";
+      break;
+    case "follow-requests":
+      url += "requestuser";
+      break;
+    case "group-events":
+      url += "event";
+      break;
+    default:
+      return undefined;
+  }
+
+  try {
+    let response = await fetch(url, {
+      credentials: "include",
+    });
+    console.log("--------->", response.status);
+
+    if (response.status === 401) {
+      //   useNavigate("/register");
+      return undefined;
+    }
+    console.log("res", response);
+    if (response.status !== 200) {
+      return undefined;
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return undefined;
+  }
 }
